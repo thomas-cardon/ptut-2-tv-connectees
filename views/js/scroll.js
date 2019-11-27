7c619e51b5a09edacf5eba9613c6a03de1d5a882
Nicolas-Rohrbach <nicolas.rohrbach@etu.univ-amu.fr>
2019-11-27 16:02:50 +0100

    Scroll vertical plus rapide

--- views/js/scroll.js
+++ views/js/scroll.js
@@ -2,7 +2,7 @@ $('.ticker1').easyTicker({
     direction: 'up',
     easing: 'swing',
     speed: 'slow',
-    interval: 5000,
+    interval: 3000,
     height: 'auto',
     visible: 0,
     mousePause: 1,
