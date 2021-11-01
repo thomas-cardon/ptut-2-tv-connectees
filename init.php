28ff4938e9904cb3a2efa5d092d5b185ad4abc79
Thomas Cardon <thomas.cardon@pm.me>
2021-11-01 19:22:01 +0100

    Init: activation error reporting dans la page (TBD)

--- init.php
+++ init.php
@@ -14,6 +14,9 @@ include 'widgets/WidgetInformation.php';
 // Login for viewer
 require 'config.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 /**
  * Create all directory
  * (For ICS file and media)
