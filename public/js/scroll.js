4a15acd51da414b02d8fce6bd02dc9a505538eec
Thomas Cardon <thomas.cardon@pm.me>
2021-11-12 15:59:01 +0100

    Script de scroll de la table si l'emploi du temps dépasse la hauteur (WIP)

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,10 +1,21 @@
-/**
- * Scroll all schedule from the bottom to the top
- */
-const ticker = Tickit({
-    data: ['item 1', 'item 2', 'item 3'],
-    selector: '#tickit',
-    duration: 10*1000,
-    initialPos: -15,
-    behavior: 'scroll'
-}).init();
+docReady(() => {
+  console.log('Scroll => Chargé');
+  const schedule = document.querySelector('.schedule');
+  const table = document.querySelector('table');
+
+  let inReverse = false;
+  
+  setInterval(() => {
+    if (schedule.clientHeight <= document.documentElement.clientHeight) return;
+    
+    let val = schedule.style.top == '' ? -1 : parseInt(schedule.style.top.slice(0, -2));
+    let max = schedule.clientHeight;
+    
+    if (Math.abs(val) == max || val == 0) inReverse = !inReverse;
+    
+    console.log(val, max, Math.abs(val) == max, inReverse, schedule.style.top);
+    
+    if (inReverse) schedule.style.top = ++val + 'px';
+    else schedule.style.top = --val + 'px';
+  }, 100);
+});
