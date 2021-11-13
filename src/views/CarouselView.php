8c925de9a6710b0e5b7946bedad293cb3a0902f1
Thomas Cardon <thomas.cardon@pm.me>
2021-11-13 21:10:28 +0100

    Le carousel d'info affichera désormais une erreur si le fichier existe pas

--- src/views/CarouselView.php
+++ src/views/CarouselView.php
@@ -6,20 +6,33 @@ class CarouselView extends View
 
     public function add($title, $content, $type): void
     {
-      $img;
-
-      switch($type) {
-        case 'pdf':
-          $img = '<object data="' . URL_PATH . TV_UPLOAD_PATH . $content . '#toolbar=0&page=1&navpanes=0&zoom=62&view=Fit&scrollbar=0" class="d-block w-100 h-100" type="application/pdf"></object>';
-          break;
-        default:
-          $img = '<img loading="lazy" src="' . URL_PATH . TV_UPLOAD_PATH . $content . '" class="d-block w-100 h-100">';
+      $data;
+      
+      if (!file_exists(PATH . TV_UPLOAD_PATH . $content)) {
+        $data = '
+          <div class="bg-warning bg-gradient w-100 h-100 text-center py-5">
+            <h1>Erreur</h1>
+            <p class="lead">Fichier introuvable</p>
+            <br />
+            <p style="text-align: left;overflow-wrap: anywhere;margin: 2rem;">
+              <samp>' . PATH . TV_UPLOAD_PATH . $content . '</samp>
+            </p>
+          </div>';
+      }
+      else {
+        switch($type) {
+          case 'pdf':
+            $data = '<object data="' . URL_PATH . TV_UPLOAD_PATH . $content . '#toolbar=0&page=1&navpanes=0&zoom=50&view=Fit&scrollbar=0" class="d-block w-100 h-100" type="application/pdf"></object>';
+            break;
+          default:
+            $data = '<img loading="lazy" src="' . URL_PATH . TV_UPLOAD_PATH . $content . '" class="d-block w-100 h-100">';
+        }
       }
             
       array_push($this->infos, '
-      <div class="carousel-item carousel-item-info' . (count($this->infos) == 0 ? ' active' : '') . '" style="min-height: 105%; min-width: 109%; overflow: hidden;">
-        ' . $img . '
-        <div class="carousel-caption d-block" style="bottom: -0.9rem">
+      <div class="carousel-item carousel-item-info' . (count($this->infos) == 0 ? ' active' : '') . '">
+        ' . $data . '
+        <div class="carousel-caption d-block bg-black opacity-50 text-white w-100 start-0 py-3" style="letter-spacing: 1.2px;">
           <h5>' . $title . '</h5>
         </div>
       </div>');
