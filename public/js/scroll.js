0057af5bc5cbbc60d7940dd53acd8a3c9df1c957
Thomas Cardon <thomas.cardon@pm.me>
2021-11-13 21:09:46 +0100

    Emploi du temps auto-scroll: s'applique sur tous les tables existantes

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,21 +1,23 @@
 docReady(() => {
   console.log('Scroll => Chargé');
-  const schedule = document.querySelector('.schedule');
-  const table = document.querySelector('table');
-
-  let inReverse = false;
   
-  setInterval(() => {
-    if (schedule.clientHeight <= document.documentElement.clientHeight) return;
+  Array.from(document.querySelectorAll('.schedule')).forEach(schedule => {
+    let inReverse = false;
     
-    let val = schedule.style.top == '' ? -1 : parseInt(schedule.style.top.slice(0, -2));
-    let max = schedule.clientHeight;
-    
-    if (Math.abs(val) == max || val == 0) inReverse = !inReverse;
-    
-    console.log(val, max, Math.abs(val) == max, inReverse, schedule.style.top);
-    
-    if (inReverse) schedule.style.top = ++val + 'px';
-    else schedule.style.top = --val + 'px';
-  }, 100);
+    setInterval(() => {
+      if (schedule.clientHeight <= document.documentElement.clientHeight) return;
+      
+      let val = schedule.style.top == '' ? -1 : parseInt(schedule.style.top.slice(0, -2));
+      let max = schedule.clientHeight;
+      
+      if (Math.abs(val) == max || val == 0) inReverse = !inReverse;
+      
+      console.log(val, max, Math.abs(val) == max, inReverse, schedule.style.top);
+      
+      if (inReverse) schedule.style.top = ++val + 'px';
+      else schedule.style.top = --val + 'px';
+    }, 100);
+
+  });
+
 });
