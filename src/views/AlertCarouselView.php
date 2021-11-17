bb6a9a0fba67350c9370778dd12a0dcc445b2147
Thomas Cardon <thomas.cardon@pm.me>
2021-11-17 11:25:16 +0100

    Les alertes ne passeront plus par dessus la météo

--- src/views/AlertCarouselView.php
+++ src/views/AlertCarouselView.php
@@ -1,22 +1,30 @@
 <?php namespace Views;
 
+/**
+ * Class AlertCarouselView
+ *
+ * Shows alerts on the home UI
+ * @author Thomas Cardon
+ * @package Views
+ */
 class AlertCarouselView extends View
 {
     private $infos = array();
 
     public function add($author, $content): void
     {
-      array_push($this->infos, $content);
+        array_push($this->infos, $content);
     }
 
     public function build(): String
     {
-        if (empty($this->infos)) return '';
-        
+        if (empty($this->infos)) {
+            return '';
+        }
+
         return '
-        <div class="marquee fixed-bottom">
+        <div class="marquee fixed-bottom" style="z-index: 0 !important;">
           <p>' . join(' — ', $this->infos) . '</p>
         </div>';
     }
 }
-?>
