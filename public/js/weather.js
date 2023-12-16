185f045798322ab863741acbd1314d5c813dee55
DE-SOUSA-GUEDES-Raphael-2225025 <shock7170@gmail.com>
2023-12-16 12:49:38 +0100

    essaie widgert

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,71 +1,43 @@
+var meteoRequest = new XMLHttpRequest();
+var longitude = 5.4510;
+var latitude = 43.5156;
+var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&lang=fr&APPID=ae546c64c1c36e47123b3d512efa723e";
+
 /**
- * refreshWeather
- * Displays the weather, updated using the last ECMAScript specs (fetch > XMLHttpRequest, especially in 2021)
- * Now includes hourly forecast
- * @author Thomas Cardon
+ * Display the weather
  */
 function refreshWeather() {
-    let myHeaders = new Headers();
-    myHeaders.append("Accept", "application/json");
-
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.lat}&lon=${weather.lon}&lang=fr&APPID=${weather.api_key}&exclude=minutely`,
-    { method: 'GET', headers: myHeaders })
-    .then(res => res.json())
-    .then(render)
-    .catch(console.error);
+    meteoRequest.open('GET', url, true);
+    meteoRequest.setRequestHeader('Accept', 'application/json');
+    meteoRequest.send();
 }
 
-function render(json) {
-    console.log('Météo -> Données chargées');
-    console.dir(json);
-
-    /**
-    * On transforme le tableau DOM primitif en réel tableau JS pour pouvoir utiliser les fonctions Array.filter(), Array.forEach
-    * On filtre les classes qui commencent par le nom gradient, et on les retire avec #forEach
-    */
-    Array.from(document.getElementById('weather-card').classList)
-    .filter(c => c.startsWith('gradient'))
-    .forEach(g => document.getElementById('weather-card').classList.remove(g));
-
-    document.getElementById('weather-card').classList.add('gradient-' + getIcon(json))
-
-    document.getElementById('temperature').innerText = Math.round(getTemp(json)) + '°C';
-    document.getElementById('wind').innerText = Math.round(getWind(json)) + ' KM/H';
-    document.getElementById('humidity').innerText = getHumidity(json) + '% humidité';
-    document.getElementById('sunset').innerText = getSunset(json);
-
-    document.getElementById('condition-icon')
-    .setAttribute(
-      'src',
-      `${weather.ASSETS_URL}/conditions/${getIcon(json)}.svg`
-    );
-
-    /* Prévisions par jour */
-    json.daily.slice(1, 6).forEach((day, i) => {
-      let d = new Date(day.dt * 1000); // Conversion unix DT vers JS DT
-
-      document.querySelector(`#forecast-d${i} strong`).innerText = d.toLocaleDateString('fr-FR', { weekday: 'short' });
-      document.querySelector(`#forecast-d${i} h6`).innerText = Math.round(kelvinToC(day.temp.day)) + '°C';
-
-      document.querySelector(`#forecast-d${i} .forecast-icon`)
-      .style
-      .backgroundImage = `url(${weather.ASSETS_URL}/conditions/${day.weather[0].icon}.svg)`;
-    });
-
-    /* Prévisions par heure */
-    json.hourly.slice(0, 5).forEach((hour, i) => {
-      let h = getHourFromTimestamp(hour.dt * 1000); // Conversion unix DT vers JS DT
-
-      document.querySelector(`#forecast-h${i} strong`).innerText = h;
-      document.querySelector(`#forecast-h${i} h6`).innerText = Math.round(kelvinToC(hour.temp)) + '°C';
-
-      document.querySelector(`#forecast-h${i} .forecast-icon`)
-        .style
-        .backgroundImage = `url(${weather.ASSETS_URL}/conditions/${hour.weather[0].icon}.svg)`;
-
-    });
-
-    setTimeout(refreshWeather, 900000);
+meteoRequest.onload = function () {
+    var json = JSON.parse(this.responseText);
+    var temp = Math.round(getTemp(json));
+    var vent = getWind(json).toFixed(0);
+    if (document.getElementById('Weather') !== null) {
+        var div = document.getElementById('Weather');
+        div.innerHTML = "";
+        var weather = document.createElement("DIV");
+        weather.innerHTML = temp + "<span class=\"degree\">°C</span>";
+        weather.id = "weather";
+        var imgTemp = document.createElement("IMG");
+        imgTemp.id = "icon";
+        imgTemp.src = "/wp-content/plugins/plugin-ecran-connecte/public/img/" + getIcon(json) + ".png";
+        imgTemp.alt = getAlt(json);
+        weather.appendChild(imgTemp);
+        var wind = document.createElement("DIV");
+        wind.innerHTML = vent + "<span class=\"kmh\">km/h</span>";
+        wind.id = "wind";
+        var imgVent = document.createElement("IMG");
+        imgVent.src = "/wp-content/plugins/plugin-ecran-connecte/public/img/wind.png";
+        imgVent.alt = "Img du vent";
+        wind.appendChild(imgVent);
+        div.appendChild(weather);
+        div.appendChild(wind);
+        setTimeout(refreshWeather, 900000);
+    }
 };
 
 /** Getter **/
@@ -73,24 +45,64 @@ function getAlt(json) {
     return json["weather"][0]["description"];
 }
 
+function getIcon(json) {
+    return cutIcon(json["weather"][0]["icon"]);
+}
+
+function cutIcon(str) {
+    return str.substr(0, str.length - 1);
+}
+
+function getTemp(json) {
+    return kelvinToC(json["main"]["temp"]);
+}
+
+function kelvinToC(kelvin) {
+    return kelvin - 273.15;
+}
+
+function getWind(json) {
+    return msToKmh(json["wind"]["speed"]);
+}
+
+function msToKmh(speed) {
+    return speed * 3.6;
+}
+/**
+ * Get the date in french format
+ */
+function dateFr() {
+    // les noms de jours / mois
+    let jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    let mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"];
+    // on recupere la date
+    let date = new Date();
+    // on construit le message
+    let message = jours[date.getDay()] + " ";   // nom du jour
+    message += date.getDate() + " ";   // numero du jour
+    message += mois[date.getMonth()] + " ";   // mois
+    message += date.getFullYear();
+    if (document.getElementById('Date') !== null)
+        document.getElementById("Date").innerHTML = message;
+}
 
 /**
- * Constant arrow functions - work for current data, not hourly forecast
+ * Get the time
  */
-const getCondition = json => json.current.weather[0].main;
-const getIcon = json => json.current.weather[0].icon;
+function heure() {
+    let date = new Date();
+    let heure = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    if (seconds < 10)
+        seconds = "0" + seconds;
+    if (document.getElementById('Time') !== null)
+        document.getElementById("Time").innerHTML = heure + ":" + minutes + ":" + seconds;
+}
 
-const getTemp = json => kelvinToC(json.current.temp);
-const getWind = json => msToKmh(json.current.wind_speed);
-const getHumidity = json => json.current.humidity;
+setInterval(dateFr, 1000);
+setInterval(heure, 1000);
 
-const getHourFromTimestamp = dt => new Date(dt * 1000).toLocaleTimeString().slice(0, 5);
-const getSunset = json => getHourFromTimestamp(json.current.sunset);
-
-const kelvinToC = k => k - 273.15;
-const msToKmh = speed => speed * 3.6;
-
-docReady(() => {
-  if (document.getElementById('temperature') == null) return;
-  refreshWeather();
-})
+refreshWeather();
--- widgets/WidgetWeather.php
+++ widgets/WidgetWeather.php
@@ -1,9 +1,8 @@
 <?php
 
 add_action('widgets_init', 'weather_register_widget');
-function weather_register_widget()
-{
-    register_widget('WidgetWeather');
+function weather_register_widget() {
+    register_widget( 'WidgetWeather');
 }
 
 /**
@@ -20,12 +19,12 @@ class WidgetWeather extends WP_Widget
     public function __construct()
     {
         parent::__construct(
-            // widget ID
+        // widget ID
             'weather_widget',
             // widget name
             __('Weather widget', ' teleconnecteeamu_widget_domain'),
             // widget description
-            array( 'description' => __('Widget qui affiche la météo', 'teleconnecteeamu_widget_domain'), )
+            array( 'description' => __( 'Widget qui affiche la météo', 'teleconnecteeamu_widget_domain' ), )
         );
     }
 
@@ -39,110 +38,16 @@ class WidgetWeather extends WP_Widget
     {
         if (is_user_logged_in()) {
             echo '
-            <script>
-              const weather = {
-                ASSETS_URL: location.origin + "' . URL_PATH . TV_PLUG_PATH . 'public/img",
-                lon: "' . WEATHER_LONGITUDE . '",
-                lat: "' . WEATHER_LATITUDE . '",
-                api_key: "' . WEATHER_API_KEY . '"
-              };
-            </script>
-            <div id="weather-card" class="flex-wrap card d-flex align-content-around flex-grow-1" style="height: 12rem;">
-              <div class="card-body" style="width: 100%">
-
-                <div id="weatherCardCarousel" class="carousel slide" style="height: 100%" data-bs-ride="carousel" data-bs-touch="false" data-bs-interval="10000">
-                  <!-- Indicators -->
-                  <div class="mb-0 carousel-indicators">
-                    <button type="button" data-bs-target="#weatherCardCarousel" data-bs-slide-to="0" class="active" aria-current="true"></button>
-                    <button type="button" data-bs-target="#weatherCardCarousel" data-bs-slide-to="1"></button>
-                    <button type="button" data-bs-target="#weatherCardCarousel" data-bs-slide-to="2"></button>
-                  </div>
-                  <!-- Carousel inner -->
-                  <div class="flex-col carousel-inner d-flex align-items-center" style="height: 100%">
-                    <div id="card-0" class="carousel-item active" style="height: 12rem;">
-                      <div class="py-2 d-flex justify-content-evenly">
-                        <div>
-                          <h2 class="display-1"><strong id="temperature">0°C</strong></h2>
-                          <small class="mb-0 fs-6">
-                            <span id="wind">0 KM/H</span>&nbsp;—&nbsp;
-                            <span id="humidity">80% humidité</span>
-                          </small>
-                          <br />
-                          <small class="mb-0 fs-6">
-                            Le soleil se couche à <span id="sunset"></span>
-                          </small>
-                        </div>
-                        <div>
-                        <img id="condition-icon" width="150px" />
-                        </div>
-                      </div>
-                    </div>
-                    <div id="card-1" class="carousel-item" style="height: 12rem;">
-                      <h3 class="my-1 text-center fs-2">Prévisions par heure</h3>
-                      <div class="py-2 my-3 text-center d-flex justify-content-between">
-                        <div id="forecast-h0" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0"><strong>25:00</strong></p>
-                        </div>
-                        <div id="forecast-h1" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0"><strong>25:00</strong></p>
-                        </div>
-                        <div id="forecast-h2" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0"><strong>25:00</strong></p>
-                        </div>
-                        <div id="forecast-h3" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0"><strong>25:00</strong></p>
-                        </div>
-                        <div id="forecast-h4" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0"><strong>25:00</strong></p>
-                        </div>
-                      </div>
-                    </div>
-                    <div id="card-2" class="carousel-item" style="height: 12rem;">
-                      <h3 class="my-1 text-center fs-2">Prévisions par jour</h3>
-                      <div class="py-2 my-3 text-center d-flex justify-content-between">
-                        <div id="forecast-d0" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0 text-capitalize"><strong>Lun.</strong></p>
-                        </div>
-                        <div id="forecast-d1" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0 text-capitalize"><strong>Mar.</strong></p>
-                        </div>
-                        <div id="forecast-d2" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0 text-capitalize"><strong>Mer.</strong></p>
-                        </div>
-                        <div id="forecast-d3" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0 text-capitalize"><strong>Jeu.</strong></p>
-                        </div>
-                        <div id="forecast-d4" class="mx-1 flex-column">
-                          <h6>99°C</h6>
-                          <div class="forecast-icon"></div>
-                          <p class="mb-0 text-capitalize"><strong>Ven.</strong></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>';
+            <aside class="Infos">
+                <p class="Time" id="Time">
+                </p>
+                <p class="Date" id="Date">
+                </p>
+                <p class="Weather" id="Weather">
+                </p>
+            </aside>';
         }
+
     }
 
     /**
@@ -154,15 +59,14 @@ class WidgetWeather extends WP_Widget
     {
         if (isset($instance['title'])) {
             $title = $instance[ 'title' ];
-        } else {
+        }
+        else {
             $title = __('Default Title', 'teleconnectee_widget_domain');
         }
 
         echo '
         <p>
-            <label for="'.$this->get_field_id('title').'">';
-        _e('Title:');
-        echo '</label>
+            <label for="'.$this->get_field_id('title').'">'; _e('Title:'); echo '</label>
             <input class="widefat" id="'.$this->get_field_id('title').'" name="'.$this->get_field_name('title').'" type="text" value="'.esc_attr($title).'" />
         </p>';
     }
@@ -179,4 +83,5 @@ class WidgetWeather extends WP_Widget
         $instance['title'] = (! empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
         return $instance;
     }
+
 }
